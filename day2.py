e6c75df2e3510b7eed393cda41be951d984b25b5
cmanh75 <npcm752004t2k29@gmail.com>
2024-10-05 00:45:17 +0700

    day-2

---
+++ day2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total = float(input("What was the total bill? $" ))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+number_of_people = int(input("How many people to split the bill? "))
+all = (total) * (1 + int(tip) / 100)
+print(f"Each person should pay: ${round(all / int(number_of_people), 2)}")
