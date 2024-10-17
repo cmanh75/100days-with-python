e78ab08a5e89dadffa122a295c257ffe42dba7a0
cmanh75 <npcm752004t2k29@gmail.com>
2024-10-17 11:56:34 +0700

    Caesar Cipher

--- day08.py
+++ day08.py
@@ -2,7 +2,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number: \n"))
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
