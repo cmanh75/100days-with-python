a4ab31a25949a4dc263145a5b9723b1edddeb548
cmanh75 <npcm752004t2k29@gmail.com>
2024-10-17 11:50:10 +0700

    Treasure Island

--- day03.py
+++ day03.py
@@ -22,7 +22,9 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+
 choice1 = input('You\'re at a the crossroad, where do you want to go? Type "left" or "right".').lower()
+
 if choice1 == "right":
     print("You fell in to a hole. Game Over.")
 else:
